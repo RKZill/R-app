f7b489806e39446ded8be9088a8a01a0d9d61fac
Ryan Kelsey <rkelsey@ontariosoilcrop.org>
2023-11-07 13:48:15 -0500

    updates:

--- app.r
+++ app.r
@@ -6,7 +6,8 @@ library(this.path)
 setwd(this.path::here())
 print(this.path::here())
 
-dat <- read.csv('/srv/shiny-server/ONFARM-soil-data.csv')
+#dat <- read.csv('/srv/shiny-server/ONFARM-soil-data.csv')
+dat <- read.csv('ONFARM-soil-data.csv')
 
 ui <- navbarPage("ONFARM Soil Health Database Tool", id='plots',
                  tabPanel("Scatterplot",
